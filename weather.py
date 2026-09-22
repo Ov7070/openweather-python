a94fc470c42d9cd63c286f4295c303b145f1fb6e
Ov7070 <merident00773k.a@gmail.com>
2026-09-22 21:26:02 +0330

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,1 +1,17 @@
-print("OpenWeather Project")
+import requests as r
+
+KEY = input("YOUR_API_KEY")
+CITY = input("Enter city")
+
+adr = "https://api.openweathermap.org/data/2.5/weather"
+
+p = {
+    "q": CITY,
+    "appid": KEY,
+    "units": "metric"
+}
+
+answer = r.get(adr, params=p)
+
+print(answer.status_code)
+print(answer.json())
